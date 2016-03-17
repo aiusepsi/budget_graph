3eeb6ca6942b80105cc85fc6cc59920a2195de82
Andrew Simpson <andy@aiusepsi.co.uk>
2016-03-17 13:54:11 +0000

    Added figures for 16/17, now compare 16/17 with 17/18 (effect only of Budget 2016)

--- budget.py
+++ budget.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 
+model15 = {"base_allowance": 10600.0, "basic_rate_band": 31785.0, "higher_rate_band" : 150000.0, "ni_threshold" : 7956.0, "ni_reduced": 41865.0}
+model16 = {"base_allowance": 11000.0, "basic_rate_band": 32000.0, "higher_rate_band" : 150000.0, "ni_threshold" : 7956.0, "ni_reduced": 41865.0}
+model17 = {"base_allowance": 11500.0, "basic_rate_band": 33500.0, "higher_rate_band" : 150000.0, "ni_threshold" : 7956.0, "ni_reduced": 41865.0}
+
 def personal_allowance(model, income):
     reduction = 0.0
     
@@ -22,6 +26,9 @@ def income_tax(model, income, personal_allowance):
     tax_amount = basic_rate_amount * 0.2 + higher_rate_amount * 0.4 + additional_rate_amount * 0.45
     
     return tax_amount
+    
+def total_income_tax(model, income):
+    return income_tax(model, income, personal_allowance(model, income))
 
 def ni(model, income):
     
@@ -79,31 +86,27 @@ def percentiles_calc():
     result = quad(fn, 0.0, 100.0)
     print result[0] * (29300000 / 100)
     
-def income_change():
+def income_change(model1, model2):
     incomes = [8370,8670,8970,9260,9570,9880,10200,10500,10700,10900,11100,11300,11600,11800,12000,12200,12400,12700,12900,13100,13300,13500,13800,14000,14200,14500,14700,14900,15200,15400,15700,15900,16100,16400,16700,16900,17200,17500,17700,18000,18300,18600,18800,19100,19400,19700,20000,20400,20700,21000,21400,21700,22100,22400,22800,23200,23500,23900,24300,24800,25200,25600,26100,26500,27000,27500,28100,28600,29100,29700,30300,30900,31500,32200,32900,33600,34300,35100,35900,36700,37700,38700,39700,40700,41800,42800,44100,45600,47200,49200,51500,54300,57700,62000,67900,76100,88000,106000,150000]
     
-    model15 = {"base_allowance": 10600.0, "basic_rate_band": 31785.0, "higher_rate_band" : 150000.0, "ni_threshold" : 7956.0, "ni_reduced": 41865.0}
-    model17 = {"base_allowance": 11500.0, "basic_rate_band": 33500.0, "higher_rate_band" : 150000.0, "ni_threshold" : 7956.0, "ni_reduced": 41865.0}
-    
-    tax15 = [total_tax(model15, i) for i in incomes]
-    tax17 = [total_tax(model17, i) for i in incomes]
-    
-    print tax15
-    print tax17
+    tax1 = [total_tax(model1, i) for i in incomes]
+    tax2 = [total_tax(model2, i) for i in incomes]
     
     change = []
     change_percent = []
 
     for i in range(0, len(incomes)):
-        change.append(tax15[i] - tax17[i])
-        percent_change = (change[i] / (incomes[i]-tax15[i]) * 100.0)
+        change.append(tax1[i] - tax2[i])
+        percent_change = (change[i] / (incomes[i]-tax1[i]) * 100.0)
         change_percent.append(percent_change)
         
     plt.plot(range(1,len(incomes)+1), change)
     plt.show()
     plt.plot(range(1,len(incomes)+1), change_percent)
     plt.show()
+
+income_change(model16, model17)
     
-income_change()
+#income_change()
 
         
